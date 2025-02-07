39d950d11c86c74df777198bb5d59176b2640d41
dMarbsOKHS <dmarbsjr08@gmail.com>
2025-02-07 14:25:05 +0000

    add index.html and css files

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,22 +27,42 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+  //toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
+  createPlatform(300, 400, 10, 350, "blue");
+  createPlatform(295, 605, 5, 1, "blue");
+  createPlatform(285, 485, 15, 1, "blue");
+  createPlatform(200, 300, 500, 10, "blue");
+  createPlatform(200, 0, 10, 300, "blue");
+  createPlatform(500, 300, 10, 300, "blue");
+  createPlatform(510, 605, 4, 4, "blue");
+  createPlatform(700, 500, 1, 1, "blue");
+  createPlatform(800, 400, 100, 10, "blue");
+  createPlatform(1100, 0, 10, 400, "blue");
+  createPlatform(1100, 540, 10, 10, "blue");
+  createPlatform(1300, 400, 5, 5, "blue") ;
+  createPlatform(1120, 300, 0.1, 0.1, "blue");
+  createPlatform(300, 100, 100, 10, "blue");
+  createPlatform(320, 230, 50, 10, "blue")
+  createPlatform(1300, 200, 100, 10, "red");
+    
+    
 
 
     // TODO 3 - Create Collectables
 
-
-
+    createCollectable("diamond", 325, 50, 0, 0)
+    createCollectable("diamond", 1325, 150)
+    createCollectable("diamond", 1325, 700)
     
     // TODO 4 - Create Cannons
-
-
+    
+    createCannon("left", 650, 500)
+    createCannon("bottom", 1300, 200)
+    createCannon("bottom", 800, 5000, 100, 20000)
+    createCannon("top", 700, 180000, 100000, 100000)
     
     
     //////////////////////////////////
